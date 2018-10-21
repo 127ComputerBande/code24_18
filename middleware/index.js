46298390aee6480962e3f360f5cf9211cb323e29
Niklas Mack <nigi.m4.a1@gmail.com>
2018-10-21 10:38:59 +0200

    Add 7tv bitches

--- middleware/index.js
+++ middleware/index.js
@@ -1,14 +1,14 @@
 const Promise = require('bluebird');
 const sportSource = require('./sources/sport');
 const youtubeSource = require('./sources/youtube');
-const galileoSource = require('./sources/galileo');
+const sevenTVSource = require('./sources/7tv');
 
 const main = async () => {
   await Promise.all([
     sportSource.getPage(sportSource.SPORT_DEUTSCHLAND_API),
     sportSource.getPage(sportSource.YOU_SPORT_API),
     youtubeSource.getVideos(),
-    galileoSource.getGalieo(),
+    sevenTVSource.get7tv(),
   ]);
 };
 
